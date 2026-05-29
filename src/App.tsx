2883127a8558c8b601737ad847b71fac1a961cde
founabil605-lang <founabil605@gmail.com>
2026-05-29 16:53:54 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import { ClubAnalytics } from "./screens/ClubAnalytics";
 export const App = () => {
   return (
     <AuthProvider>
-      <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/search" element={<SearchResults />} />
